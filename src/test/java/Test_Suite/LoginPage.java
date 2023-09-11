e467a0774ecfa7fca5478676be35a54df72d3170
MartinAle90 <martinale90@gmail.com>
2023-09-11 11:56:38 -0300

    DLYS pruebas de Automatizacion

--- src/test/java/Test_Suite/LoginPage.java
+++ src/test/java/Test_Suite/LoginPage.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.Scanner;
+
 
 public class LoginPage {
 
     Servicios ts = new Servicios();
+    Scanner input = new Scanner(System.in).useDelimiter("\n");
 
     @Test
     public void testLogin() {
@@ -28,9 +31,13 @@ public class LoginPage {
         driver.findElement(By.cssSelector("body")).click();
         driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(3)")).click();
         driver.findElement(By.id("input-with-sx")).click();
-        driver.findElement(By.id("input-with-sx")).sendKeys("1qat");
+        System.out.println("Ingrese nombre de usuario");
+        String username = input.next();
+        driver.findElement(By.id("input-with-sx")).sendKeys(username);
         driver.findElement(By.id("standard-adornment-password")).click();
-        driver.findElement(By.id("standard-adornment-password")).sendKeys("tester");
+        System.out.println("Ingrese password de usuario");
+        String password = input.next();
+        driver.findElement(By.id("standard-adornment-password")).sendKeys(password);
         driver.findElement(By.cssSelector(".MuiButton-root")).click();
 
         ts.funcionEsperar();
